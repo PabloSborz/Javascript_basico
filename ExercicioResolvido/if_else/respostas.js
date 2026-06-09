55015299f156774f1fb4ca69e45a5058fe0801f9
Pablo Sborz <linkpablosborz123@gmail.com>
2026-06-09 17:09:56 -0300

    criado um arquivo que tem Resposta do if e else

---
+++ ExercicioResolvido/if_else/respostas.js
@@ -0,0 +1,74 @@
+//Questão 1
+/*
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
+
+if (num1 > num2) {
+    console.log("O primeiro número é maior.");
+} else if (num2 > num1) {
+    console.log("O segundo número é maior.");
+} else {
+    console.log("Os números são iguais.");
+}
+*/
+//Questão 2
+/*
+let num = Number(prompt("Digite um número:"));
+if (num > 0) {
+    console.log("Positivo");
+} else if (num < 0) {
+    console.log("Negativo");
+} else {
+    console.log("Igual a zero");
+}
+*/
+//Questão 3
+/*
+let sexo = prompt("Digite seu sexo (M/F):").toUpperCase()
+if (sexo === "M") {             
+    console.log("Masculino");
+} else if (sexo === "F") {
+    console.log("Feminino");
+} else {
+    console.log("Sexo inválido.");
+}  
+*/
+//Questão 4
+/*
+let vogal = prompt("Digite uma letra:").toLowerCase();
+if (vogal === "a" || vogal === "e" || vogal === "i" || vogal === "o" || vogal === "u") {
+    console.log("É uma vogal.");
+} else {
+    console.log("Não é uma vogal.");
+}
+*/
+//Questão 5
+/*
+let nota1 = Number(prompt("Digite a primeira nota:"));
+let nota2 = Number(prompt("Digite a segunda nota:"));
+let nota3 = Number(prompt("Digite a terceira nota:"));
+let media = (nota1 + nota2 + nota3) / 3;
+if (media >= 7) {
+    console.log("Aprovado");
+}   else if (media === 10) {
+    console.log("Aprovado com distinção");
+} else {
+    console.log("Reprovado");
+}
+*/
+//Questao 6
+/*
+let num = Number(prompt("Digite um número:"));
+let num2 = Number(prompt("Digite outro número:"));
+let num3 = Number(prompt("Digite mais um número:"));
+
+if (num > num2 && num > num3) {         
+    console.log("O maior número é: " + num);
+} else if (num2 > num && num2 > num3) {
+    console.log("O maior número é: " + num2);
+} else if (num3 > num && num3 > num2) {
+    console.log("O maior número é: " + num3);
+} else {
+    console.log("Há números iguais.");
+}
+*/
